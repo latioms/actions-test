3e9fd4c79b55f6ec763b7d8c2bb9b78a7db9c7ab
latioms <73307766+latioms@users.noreply.github.com>
2023-02-12 14:30:21 +0100

    Create meteo.py

---
+++ meteo.py
@@ -0,0 +1,36 @@
+import logging
+import logging.handlers
+import os
+
+import requests
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
+if __name__ == "__main__":
+    logger.info(f"Token value: {SOME_SECRET}")
+
+    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Douala&country=CM')
+    if r.status_code == 200:
+        data = r.json()
+        temperature = data["forecast"]["temp"]
+        ressenti = data["forecast"]['feels_like']
+        humidity = data['forecast']['humidity']
+        logger.info(f'Weather in Douala: {temperature}')
