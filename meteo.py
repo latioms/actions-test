b89ce9111d75c365fe559ad08915b197d682f5a2
latioms <73307766+latioms@users.noreply.github.com>
2023-02-13 07:30:39 +0100

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -44,6 +44,6 @@ if __name__ == "__main__":
             comment = "C'est mieux que certaines fois :]"
             
         with open("meteo.txt", "w") as f:
-            f.write("TEMPERATURE={}\nHUMIDITY={}\nWEATHER_COMMENT={}".format(temperature, humidity, comment))
+            f.write("TEMPERATURE={}\nHUMIDITY={}\nFEELS_LIKE={}".format(temperature, humidity, ressenti))
             
         logger.info(f'Weather in Douala: {temperature}')
